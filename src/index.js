51c72b3da7ded43f21c92c58623c64976208f10a
Andrey Lapitskiy <70910463+BBunny19@users.noreply.github.com>
2020-10-25 03:05:57 +0300

    fix: add zero

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function toReadable (number) {
     let b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
     
     
-        if ((num = num.toString()).length > 9) return 'overflow';
+        if ((num = num.toString()).length > 9 ||  num === '0') return 'zero';
         n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
         if (!n) return; let str = '';
         str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
